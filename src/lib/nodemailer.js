f300c14c89860ac3536225b7598b604facef07ab
socktow <mjssdn95@gmail.com>
2025-07-21 07:22:39 +0700

    fix otp

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -2,11 +2,14 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  port: Number(process.env.MAIL_PORT),
   secure: process.env.MAIL_SECURE === 'true',
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // ⚠️ Chỉ dùng trong dev/test
   },
 });
 
