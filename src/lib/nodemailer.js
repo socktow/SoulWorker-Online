24b851aaff7f78bec25516035d3f8e2f951cd374
socktow <mjssdn95@gmail.com>
2025-07-21 08:42:29 +0700

    SMTP

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -1,9 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: process.env.MAIL_SECURE === 'true',
+  host: 'mail90101.maychuemail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
